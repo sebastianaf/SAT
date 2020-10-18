7e53d0ed23e52d8fd739a1fcf9d04af76dc96271
sebastianaf <sebastian.afanador@correounivalle.edu.co>
2020-10-17 20:02:02 -0500

    jar update

--- Reductor/SAT/src/reduction/Main/SatVisitor.java
+++ Reductor/SAT/src/reduction/Main/SatVisitor.java
@@ -85,7 +85,7 @@ public class SatVisitor extends SatParserBaseVisitor <String> {
                     //concateno la variable nueva
                     clause = clause.concat(String.valueOf(this.max + newVariable)).concat(" ");
                 }
-                clause = clause.concat("0 \n");
+                clause = clause.concat(" 0 \n");
             }
             //seteo mi siguiente valor maximo como el anterior mas la cantidad de variables agregadas
             this.max = this.max + this.xsat - numbers.size();
@@ -101,7 +101,7 @@ public class SatVisitor extends SatParserBaseVisitor <String> {
                 // substring de la primera clausula
                 piece = copyOfRange(auxVariables, lastindex, lastindex + this.xsat - 1);
                 strpiece = String.join(" ", piece);
-                clause = clause.concat(strpiece).concat(" ").concat(String.valueOf(this.max)).concat("0 \n");
+                clause = clause.concat(strpiece).concat(" ").concat(String.valueOf(this.max)).concat(" 0 \n");
                 lastindex = lastindex + 2;
                 //creo k-x+1 clausulas
                 for (int newClause = 2; newClause < numbers.size() - this.xsat + 1; newClause += 1) {
@@ -111,12 +111,12 @@ public class SatVisitor extends SatParserBaseVisitor <String> {
                     clause = clause.concat("-").concat(String.valueOf(this.max)).concat(" ").concat(strpiece).concat(" ").concat(String.valueOf(this.max + 1));
                     this.max = this.max + 1;
                     lastindex = lastindex + 1;
-                    clause = clause.concat("0 \n");
+                    clause = clause.concat(" 0 \n");
                 }
                 //substring de la ultima clausula
                 piece = copyOfRange(auxVariables, lastindex, lastindex + this.xsat - 1);
                 strpiece = String.join(" ", piece);
-                clause = clause.concat("-").concat(String.valueOf(this.max)).concat(" ").concat(strpiece).concat("0 \n");
+                clause = clause.concat("-").concat(String.valueOf(this.max)).concat(" ").concat(strpiece).concat(" 0 \n");
                 this.max = this.max + 1;
             }
         }
